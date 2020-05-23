b78f0f7c4159faf3f542977d421b85956d08dcb2
notsobad <tomomichi.onishi@gmail.com>
2020-05-23 18:02:33 +0900

    Add semantic like styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "next/app";
 import "../styles/tailwind.css";
+import "../styles/semantic.css.scss";
 
 export default class TailwindApp extends App {
   render() {
--- pages/blog.js
+++ pages/blog.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home({ entries }) {
   return (
-    <div className="container mx-auto my-10">
+    <div className="ui container my-10">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,14 +16,16 @@ export default function Home({ entries }) {
               ぼっちスタートアップが日々がんばっています。
             </p>
           </h1>
-
         </div>
+
+        <div className='ui button'>Button</div>
 
         <div className="grid">
           { entries.items.map((item) => (
             <a href="https://nextjs.org/docs" className="card" key={ item.sys.id }>
               <h3>{ item.fields.title }</h3>
               <p>{ item.fields.description }</p>
+              <button className="ui blue button">Follow</button>
             </a>
           )) }
         </div>
@@ -31,6 +33,12 @@ export default function Home({ entries }) {
 
       <footer>
       </footer>
+
+      <style jsx>{`
+        * {
+          font-family: "Helvetica Neue",Arial,"Hiragino Kaku Gothic ProN","Hiragino Sans",Meiryo,sans-serif;
+        }
+      `}</style>
     </div>
   )
 }
