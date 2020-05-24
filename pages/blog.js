2c04caec6aadc2b432bff2b694353d1795bac7ef
notsobad <tomomichi.onishi@gmail.com>
2020-05-24 16:54:05 +0900

    test article layout

--- pages/blog.js
+++ pages/blog.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 
 export default function Home({ entries }) {
   return (
-    <div className="ui container my-10">
+    <div className="ui container mb-10 mt-5 mg:mt-10">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <div className='mb-10'>
+        <div className='mb-16'>
           <h1 className="text-3xl font-bold mb-4">
             NOT SO BADなブログ
             <p className="text-sm text-gray-600 font-thin">
@@ -18,15 +18,22 @@ export default function Home({ entries }) {
           </h1>
         </div>
 
-        <div className='ui button'>Button</div>
-
-        <div className="grid">
+        <div>
           { entries.items.map((item) => (
-            <a href="https://nextjs.org/docs" className="card" key={ item.sys.id }>
-              <h3>{ item.fields.title }</h3>
-              <p>{ item.fields.description }</p>
-              <button className="ui blue button">Follow</button>
-            </a>
+            <div className="md:flex mb-12" key={ item.sys.id }>
+              <div className='md:w-1/4'>
+                <img src={ item.fields.heroImage.fields.file.url + '?w=500' } />
+              </div>
+              <div className='md:ml-4'>
+                <div className='md:mb-4'>
+                  <h3 className='text-lg font-bold'>{ item.fields.title }</h3>
+                  { item.fields.tags.map((tag) => (
+                    <span className='mr-2 text-gray-500'>{tag}</span>
+                  )) }
+                </div>
+                <p>{ item.fields.description }</p>
+              </div>
+            </div>
           )) }
         </div>
       </main>
@@ -49,7 +56,7 @@ export async function getStaticProps(context) {
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
   })
 
-  const entries = await client.getEntries({ 'content_type': 'post' })
+  const entries = await client.getEntries({ 'content_type': 'blogPost' })
   .then(function (entries) {
     const items = entries.items;
     console.log(items)
