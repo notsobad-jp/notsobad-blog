70fca6f79352725ef95fa220cce1a8f5783f4493
notsobad <tomomichi.onishi@gmail.com>
2020-05-30 18:09:10 +0900

    Fix company page

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ export default function Home({ entries }) {
       </Head>
 
       <section className="text-gray-700 body-font">
-        <div className="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
+        <div className="container mx-auto flex px-5 py-12 md:py-24 items-center justify-center flex-col">
           <h1>
-            <img className="lg:w-3/6 md:w-4/6 w-6/6 mb-10 object-cover object-center rounded mx-auto" alt="NOT SO BAD" src="/images/logo.png" />
+            <img className="lg:w-3/6 md:w-4/6 w-6/6 mb-10 object-cover object-center rounded mx-auto" alt="NOT SO BAD" src="/images/notsobad/logo.png" />
           </h1>
           <div className="text-center lg:w-2/3 w-full">
-            <p className="title-font sm:text-xl text-3xl mb-4 text-gray-900">Towards a not so bad world.</p>
+            <p className="title-font text-2xl md:text-3xl mb-4 text-gray-900">Towards a not so bad world.</p>
           </div>
         </div>
       </section>
@@ -32,18 +32,11 @@ export default function Home({ entries }) {
 
       <section className="text-gray-700 body-font bg-gray-200">
         <div className="container px-5 py-24 mx-auto">
-          <div className="text-center mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Raw Denim Heirloom Man Braid</h1>
-            <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug.</p>
-            <div className="flex mt-6 justify-center">
-              <div className="w-16 h-1 rounded-full bg-teal-500 inline-flex"></div>
-            </div>
-          </div>
           <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-            <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col text-center items-center">
-              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-100 text-gray-500 mb-5 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+            <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col flex-grow text-center items-center">
+              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-100 text-gray-800 mb-5 flex-shrink-0">
+                <svg fill="currentColor" stroke="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 20 20">
+                  <path d="M12 10a2 2 0 01-3.41 1.41A2 2 0 0110 8V0a9.97 9.97 0 0110 10h-8zm7.9 1.41A10 10 0 118.59.1v2.03a8 8 0 109.29 9.29h2.02zm-4.07 0a6 6 0 11-7.25-7.25v2.1a3.99 3.99 0 00-1.4 6.57 4 4 0 006.56-1.42h2.1z"/>
                 </svg>
               </div>
               <div className="flex-grow">
@@ -51,12 +44,10 @@ export default function Home({ entries }) {
                 <p className="leading-relaxed text-base">Keep focusing on your favourite niche market.</p>
               </div>
             </div>
-            <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col text-center items-center">
-              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-100 text-gray-500 mb-5 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
-                  <circle cx="6" cy="6" r="3"></circle>
-                  <circle cx="6" cy="18" r="3"></circle>
-                  <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+            <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col flex-grow text-center items-center">
+              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-100 text-gray-800 mb-5 flex-shrink-0">
+                <svg fill="currentColor" stroke="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 20a10 10 0 110-20 10 10 0 010 20zm0-18a8 8 0 100 16 4 4 0 110-8 4 4 0 100-8zm0 13a1 1 0 100-2 1 1 0 000 2zm0-8a1 1 0 110-2 1 1 0 010 2z"/>
                 </svg>
               </div>
               <div className="flex-grow">
@@ -64,11 +55,10 @@ export default function Home({ entries }) {
                 <p className="leading-relaxed text-base">Do what other people never do.</p>
               </div>
             </div>
-            <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col text-center items-center">
-              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-100 text-gray-500 mb-5 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
-                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
+            <div className="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col flex-grow text-center items-center">
+              <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-100 text-gray-800 mb-5 flex-shrink-0">
+                <svg fill="currentColor" stroke="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 20 20">
+                  <path d="M10 20a10 10 0 110-20 10 10 0 010 20zm0-2a8 8 0 100-16 8 8 0 000 16zM6.5 9a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm7 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm2.16 3a6 6 0 01-11.32 0h11.32z"/>
                 </svg>
               </div>
               <div className="flex-grow">
@@ -86,7 +76,7 @@ export default function Home({ entries }) {
           <div className="flex flex-wrap -m-4 mb-10">
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog" />
+                <img className="lg:h-64 md:h-56 w-full object-cover object-center" src="/images/notsobad/products/bungomail.png" alt="bungomail" />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">CATEGORY</h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
@@ -115,7 +105,7 @@ export default function Home({ entries }) {
             </div>
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog" />
+                <img className="lg:h-64 md:h-56 w-full object-cover object-center" src="/images/notsobad/products/bungosearch.png" alt="bungosearch" />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">CATEGORY</h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
@@ -144,7 +134,7 @@ export default function Home({ entries }) {
             </div>
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog" />
+                <img className="lg:h-64 md:h-56 w-full object-cover object-center" src="/images/notsobad/products/timeline.png" alt="THE TIMELINE" />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">CATEGORY</h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
@@ -176,7 +166,7 @@ export default function Home({ entries }) {
           <div className="flex flex-wrap -m-4 mb-10">
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog" />
+                <img className="lg:h-64 md:h-56 w-full object-cover object-center" src="/images/notsobad/products/tournament.png" alt="THE TOURNAMENT" />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">CATEGORY</h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
@@ -205,7 +195,7 @@ export default function Home({ entries }) {
             </div>
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog" />
+                <img className="lg:h-64 md:h-56 w-full object-cover object-center" src="/images/notsobad/products/tabsekki.png" alt="tabsekki" />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">CATEGORY</h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
@@ -234,7 +224,7 @@ export default function Home({ entries }) {
             </div>
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog" />
+                <img className="lg:h-64 md:h-56 w-full object-cover object-center" src="/images/notsobad/products/spkarta.png" alt="スマホde百人一首" />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">CATEGORY</h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
@@ -271,7 +261,7 @@ export default function Home({ entries }) {
           <div className="flex flex-wrap -m-4">
             <div className="p-4 lg:w-1/2 mx-auto">
               <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200" />
+                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="/images/notsobad/profile.png" alt='CEO' />
                 <div className="flex-grow sm:pl-8">
                   <h2 className="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
                   <h3 className="text-gray-500 mb-3">UI Developer</h3>
@@ -305,38 +295,26 @@ export default function Home({ entries }) {
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:w-2/3 w-full mx-auto overflow-auto">
             <table className="table-auto w-full text-left whitespace-no-wrap">
-              <thead>
-                <tr>
-                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200 rounded-tl rounded-bl">Plan</th>
-                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200">Speed</th>
-                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200">Storage</th>
-                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200">Price</th>
-                </tr>
-              </thead>
               <tbody>
                 <tr>
-                  <td className="px-4 py-3">Start</td>
-                  <td className="px-4 py-3">5 Mb/s</td>
-                  <td className="px-4 py-3">15 GB</td>
-                  <td className="px-4 py-3 text-lg text-gray-900">Free</td>
+                  <td className="border-t-2 border-gray-200 px-4 py-3">Company</td>
+                  <td className="border-t-2 border-gray-200 px-4 py-3">NOT SO BAD, LLC.</td>
                 </tr>
                 <tr>
-                  <td className="border-t-2 border-gray-200 px-4 py-3">Pro</td>
-                  <td className="border-t-2 border-gray-200 px-4 py-3">25 Mb/s</td>
-                  <td className="border-t-2 border-gray-200 px-4 py-3">25 GB</td>
-                  <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$24</td>
+                  <td className="border-t-2 border-gray-200 px-4 py-3">Established</td>
+                  <td className="border-t-2 border-gray-200 px-4 py-3">22 March 2018</td>
                 </tr>
                 <tr>
-                  <td className="border-t-2 border-gray-200 px-4 py-3">Business</td>
-                  <td className="border-t-2 border-gray-200 px-4 py-3">36 Mb/s</td>
-                  <td className="border-t-2 border-gray-200 px-4 py-3">40 GB</td>
-                  <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$50</td>
+                  <td className="border-t-2 border-gray-200 px-4 py-3">CEO</td>
+                  <td className="border-t-2 border-gray-200 px-4 py-3">Tomomichi Onishi</td>
                 </tr>
                 <tr>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">Exclusive</td>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">48 Mb/s</td>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">120 GB</td>
-                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$72</td>
+                  <td className="border-t-2 border-gray-200 px-4 py-3">Address</td>
+                  <td className="border-t-2 border-gray-200 px-4 py-3">4-16-6 Koishi-kawa, Bunkyo, Tokyo</td>
+                </tr>
+                <tr>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">Contact</td>
+                  <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">Mail: info[at]notsobad.jp</td>
                 </tr>
               </tbody>
             </table>
@@ -348,35 +326,12 @@ export default function Home({ entries }) {
         <div className="bg-gray-900">
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-200">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-teal-500 rounded-full" viewBox="0 0 24 24">
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-              </svg>
-              <span className="ml-3 text-xl">madde</span>
+              <span className="ml-3 text-xl">NOT SO BAD, LLC.</span>
             </a>
-            <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 madde —
-              <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
-            </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-              <a className="text-gray-500">
-                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-500">
+              <a href='https://twitter.com/kame_f_no7' target='_blank' className="ml-3 text-gray-500">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-500">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                </svg>
-              </a>
-              <a className="ml-3 text-gray-500">
-                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
-                  <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
                 </svg>
               </a>
             </span>
